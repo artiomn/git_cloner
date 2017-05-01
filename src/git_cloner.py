b6527b2bcbb69b2420a4778e781820010f0be290
artiom <artiom14@yandex.ru>
2017-05-01 15:27:51 +0300

    Bitbucket cloner enabled

--- src/git_cloner.py
+++ src/git_cloner.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('--type', '-t', type=str, nargs=1, help='service type: `bitbucket` or `github`')
     parser.add_argument('--login', '-l', type=str, nargs=1, default=[''], help='user login')
     parser.add_argument('--password', '-p', type=str, nargs=1, default=[''], help='user password')
-    parser.add_argument('--owner', '-o', type=str, nargs=1, default=[''], help='projects owner')
+    parser.add_argument('--owner', '-o', type=str, nargs=1, default=[''], help='repositories set owner')
 
     parser.add_argument('site', metavar='site', type=str, help='service address')
 
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     site = args.site
     login = args.login[0]
     password = args.password[0]
+    owner = args.owner[0]
 
     if service_type is None:
         if 'github' in site:
@@ -29,11 +30,11 @@ if __name__ == '__main__':
 
     if service_type == 'bitbucket':
         from git_cloner.bitbucket import BitbucketCloner
+        BitbucketCloner(site, owner=owner, login=login, password=password).clone()
     elif service_type == 'github':
         from git_cloner.github import GithubCloner
-        owner = args.owner[0]
         GithubCloner(site, owner=owner, login=login, password=password).clone()
     else:
-        print('Unknown type "{}"!'.format(service_type))
+        print('Unknown service provider type "{}"!'.format(service_type))
         exit(1)
 
