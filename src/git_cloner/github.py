bf94fe82277d06aa3564c70b02b370d1a9b08d67
artiom <artiom14@yandex.ru>
2017-05-01 20:01:26 +0300

    - Possibility to download all projects of all users from Github added. - Method `_download_path` added.

--- src/git_cloner/github.py
+++ src/git_cloner/github.py
@@ -21,9 +21,15 @@ class GithubCloner(object):
         self.site = site
         self.owner = owner
 
+    def _download_path(self, path):
+        return connect_with_auth('api.{}'.format(self.site),
+                                 path=path,
+                                 login=self.login,
+                                 password=self.password).decode('utf-8')
+
     def _clone_repo(self, repo):
         repo_name = repo['name']
-        result_path = repo_name
+        result_path = '{}/{}'.format(self.owner, repo_name)
 
         try:
             clone_url = repo['clone_url']
@@ -41,21 +47,48 @@ class GithubCloner(object):
             print('Cloning "{}"...'.format(result_path))
             git.Git().clone(clone_url, result_path)
 
-    def _clone_projects(self):
-        repos = connect_with_auth('api.{}'.format(self.site), path='/users/{}/repos'.format(self.owner),
-                                  login=self.login, password=self.password).\
-            decode('utf-8')
+    def _get_user_list(self, since):
+        users = self._download_path('/users?since={}'.format(since))
+
+        if not isinstance(users, list):
+            raise GithubClonerException(users)
+
+        return users
+
+    def _clone_user_projects(self):
+        repos = self._download_path('/users/{}/repos'.format(self.owner))
 
         repos = json.loads(repos)
 
-        if not  isinstance(repos, list):
+        if not isinstance(repos, list):
             raise GithubClonerException(repos)
 
-        with open('repos.json', 'w') as f:
+        if not os.path.isdir(self.owner):
+            os.mkdir(self.owner)
+
+        with open('{}/repos.json'.format(self.owner), 'w') as f:
             pprint(repos, stream=f)
 
         for repo in repos:
             self._clone_repo(repo)
 
+    def _clone_all_projects(self):
+        since = 0
+        users = self._get_user_list(since)
+
+        while users:
+            if not isinstance(users, list):
+                raise GithubClonerException(users)
+
+            for user in users:
+                self.owner = user.get('login')
+                self._clone_user_projects()
+
+            users = self._get_user_list(since)
+            since += len(users)
+
     def clone(self):
-        return self._clone_projects()
+        if self.owner:
+            return self._clone_user_projects()
+        else:
+            return self._clone_all_projects()
